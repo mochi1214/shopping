7ac3a278e5769e6dc6dda54d68514103930790d8
mochi <n1046746@ntub.edu.tw>
2019-02-11 21:52:32 +0800

    Initial Commit

---
+++ shopping/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shopping
+//
+//  Created by 吳佳臻 on 2019/2/11.
+//  Copyright © 2019 ChiaChen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
